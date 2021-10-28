041f86182d80abb8bc1db0a27b81c27599f2e962
Nabaa Jafar <66154604+nabaajafar@users.noreply.github.com>
2021-10-28 13:24:24 +0300

    Add files via upload

---
+++ EMKAN.Trace/Abstraction/IRequestTrace.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMKAN.Trace.Abstraction
+{
+    public interface IRequestTrace
+    {
+        Guid Id { get; }
+        DateTime TimeStamp { get; }
+        void Register(Guid id);
+    }
+}
---
+++ EMKAN.Trace/Service/RequestTrace.cs
@@ -0,0 +1,23 @@
+﻿using EMKAN.Trace.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EMKAN.Trace.Service
+{
+    public class RequestTrace : IRequestTrace
+    {
+        public Guid Id { get; protected set; }
+
+        public DateTime TimeStamp { get; protected set; }
+
+        public RequestTrace()
+        {
+            TimeStamp = DateTime.Now;
+        }
+        public void Register(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
